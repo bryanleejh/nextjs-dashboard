4a36f44aecc17ceaebb68ebdee498328e68493f4
Bryan Lee <bryan.lee.jh@gmail.com>
2024-12-20 17:57:27 +0700

    feat: add authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -60,6 +62,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
+    console.log(error);
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -98,6 +101,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
   } catch (error) {
+    console.log(error);
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
@@ -113,5 +117,24 @@ export async function deleteInvoice(id: string) {
   } catch (error) {
     console.log(error);
     console.log("Database Error: Failed to Delete Invoice.");
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
